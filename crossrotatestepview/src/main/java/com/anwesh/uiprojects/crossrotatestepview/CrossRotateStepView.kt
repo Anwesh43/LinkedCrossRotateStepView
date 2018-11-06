571669e744594c81eca6a9e58c0c0d781d34c6e7
Anwesh43 <anweshthecool0@gmail.com>
2018-11-06 11:53:25 +0530

    adding drawCSRNode canvas extension method

--- crossrotatestepview/src/main/java/com/anwesh/uiprojects/crossrotatestepview/CrossRotateStepView.kt
+++ crossrotatestepview/src/main/java/com/anwesh/uiprojects/crossrotatestepview/CrossRotateStepView.kt
@@ -26,4 +26,26 @@ fun Float.getMirrorValue(a : Float, b : Float) : Float = this * a + (1 - this) *
 
 fun Float.updateScale(dir : Float) = scGap * dir * getScaleFactor().getMirrorValue(1f, lines.getInverse())
 
-fun Float.divideScale(i : Int, n : Int) = Math.min(n.getInverse(), Math.max(0f, this - n.getInverse() * i))
+fun Float.divideScale(i : Int, n : Int) = Math.min(n.getInverse(), Math.max(0f, this - n.getInverse() * i)) * n
+
+fun Canvas.drawCRSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / 3
+    val deg : Float = 360f / nodes
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.color = Color.parseColor("#4CAF50")
+    save()
+    translate(gap * (i + 1), h/2)
+    rotate(deg * sc2)
+    for (j in 0..(lines - 1)) {
+        val sc : Float = sc1.divideScale(j, lines)
+        save()
+        rotate(deg * j)
+        drawLine(size - size * sc, 0f, size, 0f, paint)
+        restore()
+    }
+    restore()
+}
