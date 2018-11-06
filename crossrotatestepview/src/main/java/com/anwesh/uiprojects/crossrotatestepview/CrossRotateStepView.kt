0b336c639e6e6f81434bf2580b0b30040eb5632d
Anwesh43 <anweshthecool0@gmail.com>
2018-11-06 12:14:23 +0530

    adding CrossRotateStep class

--- crossrotatestepview/src/main/java/com/anwesh/uiprojects/crossrotatestepview/CrossRotateStepView.kt
+++ crossrotatestepview/src/main/java/com/anwesh/uiprojects/crossrotatestepview/CrossRotateStepView.kt
@@ -163,4 +163,28 @@ class CrossRotateStepView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class CrossRotateStep(var i : Int) {
+
+        private var curr : CRSNode = CRSNode(0)
+
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
