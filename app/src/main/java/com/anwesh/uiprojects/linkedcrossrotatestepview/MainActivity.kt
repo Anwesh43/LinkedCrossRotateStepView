04e222288d8c52e6059507d5aab2690fab3754eb
Anwesh43 <anweshthecool0@gmail.com>
2018-11-06 12:34:43 +0530

    adding OnAnimationCompleteListener in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedcrossrotatestepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedcrossrotatestepview/MainActivity.kt
@@ -3,14 +3,24 @@ package com.anwesh.uiprojects.linkedcrossrotatestepview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.crossrotatestepview.CrossRotateStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        CrossRotateStepView.create(this)
+        val view : CrossRotateStepView = CrossRotateStepView.create(this)
         fullScreen()
+        view.addOnAnimationCompleteListener({
+            createToast("animation number $it completed")
+        }) {
+            createToast("animation number $it is reset")
+        }
+    }
+
+    private fun createToast(txt : String) {
+        Toast.makeText(this, txt, Toast.LENGTH_SHORT).show()
     }
 }
 
