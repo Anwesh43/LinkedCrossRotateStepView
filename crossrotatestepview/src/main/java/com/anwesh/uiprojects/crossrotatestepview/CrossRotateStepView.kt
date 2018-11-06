d4a429c2087b85f3f37eaeb3e2a491b3a3de5a24
Anwesh43 <anweshthecool0@gmail.com>
2018-11-06 12:07:47 +0530

    adding CRSNode class

--- crossrotatestepview/src/main/java/com/anwesh/uiprojects/crossrotatestepview/CrossRotateStepView.kt
+++ crossrotatestepview/src/main/java/com/anwesh/uiprojects/crossrotatestepview/CrossRotateStepView.kt
@@ -118,4 +118,49 @@ class CrossRotateStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CRSNode(var i : Int, val state : State = State()) {
+
+        private var next : CRSNode? = null
+
+        private var prev : CRSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = CRSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCRSNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CRSNode {
+            var curr : CRSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
