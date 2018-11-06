6a294f2b18f19881a25a9e7400af46f219277f31
Anwesh43 <anweshthecool0@gmail.com>
2018-11-06 11:47:32 +0530

    adding Float and Int extension methods

--- crossrotatestepview/src/main/java/com/anwesh/uiprojects/crossrotatestepview/CrossRotateStepView.kt
+++ crossrotatestepview/src/main/java/com/anwesh/uiprojects/crossrotatestepview/CrossRotateStepView.kt
@@ -15,3 +15,15 @@ import android.view.MotionEvent
 val nodes : Int = 5
 
 val lines : Int = 4
+
+val scGap : Float = 0.05f
+
+fun Int.getInverse() : Float = (1f / this)
+
+fun Float.getScaleFactor() : Float = Math.floor(this / 0.5).toFloat()
+
+fun Float.getMirrorValue(a : Float, b : Float) : Float = this * a + (1 - this) * b
+
+fun Float.updateScale(dir : Float) = scGap * dir * getScaleFactor().getMirrorValue(1f, lines.getInverse())
+
+fun Float.divideScale(i : Int, n : Int) = Math.min(n.getInverse(), Math.max(0f, this - n.getInverse() * i))
