c6e5633f8f8bea540765e0f7c00d54fc5b00d0f1
Anwesh43 <anweshthecool0@gmail.com>
2018-11-06 11:39:05 +0530

    adding libraries and defining nodes

--- crossrotatestepview/src/main/java/com/anwesh/uiprojects/crossrotatestepview/CrossRotateStepView.kt
+++ crossrotatestepview/src/main/java/com/anwesh/uiprojects/crossrotatestepview/CrossRotateStepView.kt
@@ -3,3 +3,15 @@ package com.anwesh.uiprojects.crossrotatestepview
 /**
  * Created by anweshmishra on 06/11/18.
  */
+
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+import android.view.View
+import android.view.MotionEvent
+
+val nodes : Int = 5
+
+val lines : Int = 4
